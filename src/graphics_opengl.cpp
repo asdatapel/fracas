ecebd5b421c304cc5cbb5ee70c33e474a1f89734
unknown <asdatapel@gmail.com>
2021-03-08 21:46:47 -0600

    tiny refactor

--- src/graphics_opengl.cpp
+++ src/graphics_opengl.cpp
@@ -17,28 +17,10 @@ unsigned int temp_fbo;
 unsigned int temp_rbo;
 
 unsigned int basic_shader;
-static int basic_shader_resolution_uniform;
-static int basic_shader_pos_uniform;
-static int basic_shader_scale_uniform;
-static int basic_shader_color_uniform;
-
 unsigned int textured_shader;
-static int textured_shader_resolution_uniform;
-static int textured_shader_pos_uniform;
-static int textured_shader_scale_uniform;
-static int textured_shader_color_uniform;
-
 unsigned int textured_mapped_shader;
-static int textured_mapped_shader_resolution_uniform;
-static int textured_mapped_shader_pos_uniform;
-static int textured_mapped_shader_scale_uniform;
-static int textured_mapped_shader_uv_uniform;
-
 unsigned int threed_shader;
-static int threed_shader_t_uniform;
-
 unsigned int bar_shader;
-
 unsigned int rect_to_cubemap_shader;
 unsigned int cubemap_shader;
 unsigned int irradiance_shader;
@@ -176,24 +158,6 @@ static void init_graphics(RenderTarget target)
     glFrontFace(GL_CCW);
     glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 
-    basic_shader = create_shader_program("resources/shaders/basic");
-    basic_shader_resolution_uniform = glGetUniformLocation(basic_shader, "resolution");
-    basic_shader_pos_uniform = glGetUniformLocation(basic_shader, "pos");
-    basic_shader_scale_uniform = glGetUniformLocation(basic_shader, "scale");
-    basic_shader_color_uniform = glGetUniformLocation(basic_shader, "color");
-
-    textured_shader = create_shader_program("resources/shaders/textured");
-    textured_shader_resolution_uniform = glGetUniformLocation(textured_shader, "resolution");
-    textured_shader_pos_uniform = glGetUniformLocation(textured_shader, "pos");
-    textured_shader_scale_uniform = glGetUniformLocation(textured_shader, "scale");
-    textured_shader_color_uniform = glGetUniformLocation(textured_shader, "color");
-
-    textured_mapped_shader = create_shader_program("resources/shaders/font_atlas");
-    textured_mapped_shader_resolution_uniform = glGetUniformLocation(textured_mapped_shader, "resolution");
-    textured_mapped_shader_pos_uniform = glGetUniformLocation(textured_mapped_shader, "pos");
-    textured_mapped_shader_scale_uniform = glGetUniformLocation(textured_mapped_shader, "scale");
-    textured_mapped_shader_uv_uniform = glGetUniformLocation(textured_mapped_shader, "uv");
-
     glGenVertexArrays(1, &screen_quad_vao);
     glGenBuffers(1, &screen_quad_vbo);
     glBindVertexArray(screen_quad_vao);
@@ -204,17 +168,6 @@ static void init_graphics(RenderTarget target)
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
 
-    threed_shader = create_shader_program("resources/shaders/threed");
-    threed_shader_t_uniform = glGetUniformLocation(threed_shader, "t");
-
-    bar_shader = create_shader_program("resources/shaders/bar");
-
-    rect_to_cubemap_shader = create_shader_program("resources/shaders/rect_to_cubemap");
-    cubemap_shader = create_shader_program("resources/shaders/cubemap");
-    irradiance_shader = create_shader_program("resources/shaders/irradiance");
-    env_filter_shader = create_shader_program("resources/shaders/env_filter");
-    brdf_lut_shader = create_shader_program("resources/shaders/brdf_lut");
-
     glGenVertexArrays(1, &cube_vao);
     glGenBuffers(1, &cube_vbo);
     glBindBuffer(GL_ARRAY_BUFFER, cube_vbo);
@@ -224,134 +177,22 @@ static void init_graphics(RenderTarget target)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+    basic_shader = create_shader_program("resources/shaders/basic");
+    textured_shader = create_shader_program("resources/shaders/textured");
+    textured_mapped_shader = create_shader_program("resources/shaders/font_atlas");
+    threed_shader = create_shader_program("resources/shaders/threed");
+    bar_shader = create_shader_program("resources/shaders/bar");
+    rect_to_cubemap_shader = create_shader_program("resources/shaders/rect_to_cubemap");
+    cubemap_shader = create_shader_program("resources/shaders/cubemap");
+    irradiance_shader = create_shader_program("resources/shaders/irradiance");
+    env_filter_shader = create_shader_program("resources/shaders/env_filter");
+    brdf_lut_shader = create_shader_program("resources/shaders/brdf_lut");
 }
 
 static void clear_backbuffer()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-}
-
-static void draw_threed(RenderTarget target, Camera camera, glm::mat4 model, VertexBuffer buf, Material material, Texture cubemap, Texture env_map, Texture brdf_lut)
-{
-    glUseProgram(threed_shader);
-
-    static float t = 0.0f;
-    t += 0.01f;
-    glUniform1f(threed_shader_t_uniform, t);
-
-    glUniformMatrix4fv(glGetUniformLocation(threed_shader, "model"), 1, GL_FALSE, &model[0][0]);
-    glUniformMatrix4fv(glGetUniformLocation(threed_shader, "view"), 1, GL_FALSE, &camera.view[0][0]);
-    glUniformMatrix4fv(glGetUniformLocation(threed_shader, "projection"), 1, GL_FALSE, &camera.perspective[0][0]);
-    glUniform3f(glGetUniformLocation(threed_shader, "cam_pos"), camera.pos_x, camera.pos_y, camera.pos_z);
-
-    glUniform1i(glGetUniformLocation(threed_shader, "albedo_tex"), 0);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, material.albedo.gl_reference);
-    glUniform1i(glGetUniformLocation(threed_shader, "normal_tex"), 1);
-    glActiveTexture(GL_TEXTURE0 + 1);
-    glBindTexture(GL_TEXTURE_2D, material.normal.gl_reference);
-    glUniform1i(glGetUniformLocation(threed_shader, "metal_tex"), 2);
-    glActiveTexture(GL_TEXTURE0 + 2);
-    glBindTexture(GL_TEXTURE_2D, material.metal.gl_reference);
-    glUniform1i(glGetUniformLocation(threed_shader, "roughness_tex"), 3);
-    glActiveTexture(GL_TEXTURE0 + 3);
-    glBindTexture(GL_TEXTURE_2D, material.roughness.gl_reference);
-    glUniform1i(glGetUniformLocation(threed_shader, "irradiance"), 4);
-    glActiveTexture(GL_TEXTURE0 + 4);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap.gl_reference);
-    glUniform1i(glGetUniformLocation(threed_shader, "env"), 5);
-    glActiveTexture(GL_TEXTURE0 + 5);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, env_map.gl_reference);
-    glUniform1i(glGetUniformLocation(threed_shader, "brdf"), 6);
-    glActiveTexture(GL_TEXTURE0 + 6);
-    glBindTexture(GL_TEXTURE_2D, brdf_lut.gl_reference);
-
-    glBindVertexArray(buf.vao);
-    glDrawArrays(GL_TRIANGLES, 0, buf.vert_count);
-}
-
-static void draw_bar(RenderTarget target, Camera camera, glm::mat4 model, VertexBuffer buf, Material material, Texture cubemap, Texture env_map, Texture brdf_lut, Texture text)
-{
-    glUseProgram(bar_shader);
-
-    static float t = 0.0f;
-    t += 0.01f;
-    glUniform1f(glGetUniformLocation(bar_shader, "t"), t);
-
-    glUniformMatrix4fv(glGetUniformLocation(bar_shader, "model"), 1, GL_FALSE, &model[0][0]);
-    glUniformMatrix4fv(glGetUniformLocation(bar_shader, "view"), 1, GL_FALSE, &camera.view[0][0]);
-    glUniformMatrix4fv(glGetUniformLocation(bar_shader, "projection"), 1, GL_FALSE, &camera.perspective[0][0]);
-    glUniform3f(glGetUniformLocation(bar_shader, "cam_pos"), camera.pos_x, camera.pos_y, camera.pos_z);
-
-    glUniform1i(glGetUniformLocation(bar_shader, "albedo_tex"), 0);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, material.albedo.gl_reference);
-    glUniform1i(glGetUniformLocation(bar_shader, "normal_tex"), 1);
-    glActiveTexture(GL_TEXTURE0 + 1);
-    glBindTexture(GL_TEXTURE_2D, material.normal.gl_reference);
-    glUniform1i(glGetUniformLocation(bar_shader, "metal_tex"), 2);
-    glActiveTexture(GL_TEXTURE0 + 2);
-    glBindTexture(GL_TEXTURE_2D, material.metal.gl_reference);
-    glUniform1i(glGetUniformLocation(bar_shader, "roughness_tex"), 3);
-    glActiveTexture(GL_TEXTURE0 + 3);
-    glBindTexture(GL_TEXTURE_2D, material.roughness.gl_reference);
-    glUniform1i(glGetUniformLocation(bar_shader, "irradiance"), 4);
-    glActiveTexture(GL_TEXTURE0 + 4);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap.gl_reference);
-    glUniform1i(glGetUniformLocation(bar_shader, "env"), 5);
-    glActiveTexture(GL_TEXTURE0 + 5);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, env_map.gl_reference);
-    glUniform1i(glGetUniformLocation(bar_shader, "brdf"), 6);
-    glActiveTexture(GL_TEXTURE0 + 6);
-    glBindTexture(GL_TEXTURE_2D, brdf_lut.gl_reference);
-    glUniform1i(glGetUniformLocation(bar_shader, "text_tex"), 7);
-    glActiveTexture(GL_TEXTURE0 + 7);
-    glBindTexture(GL_TEXTURE_2D, text.gl_reference);
-
-    glBindVertexArray(buf.vao);
-    glDrawArrays(GL_TRIANGLES, 0, buf.vert_count);
-}
-
-static void draw_rect(RenderTarget target, Rect rect, Color color)
-{
-    glUseProgram(basic_shader);
-    glUniform2i(basic_shader_resolution_uniform, target.width, target.height);
-    glUniform2f(basic_shader_pos_uniform, rect.x, rect.y);
-    glUniform2f(basic_shader_scale_uniform, rect.width, rect.height);
-    glUniform4f(basic_shader_color_uniform, color.r, color.g, color.b, color.a);
-
-    glBindVertexArray(screen_quad_vao);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-}
-
-static void draw_textured_rect(RenderTarget target, Rect rect, Color color, Texture tex)
-{
-    glUseProgram(textured_shader);
-    glUniform2i(textured_shader_resolution_uniform, target.width, target.height);
-    glUniform2f(textured_shader_pos_uniform, rect.x, rect.y);
-    glUniform2f(textured_shader_scale_uniform, rect.width, rect.height);
-    glUniform4f(textured_shader_color_uniform, color.r, color.g, color.b, color.a);
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, tex.gl_reference);
-
-    glBindVertexArray(screen_quad_vao);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-}
-
-static void draw_textured_mapped_rect(RenderTarget target, Rect rect, Rect uv, Texture tex)
-{
-    glUseProgram(textured_mapped_shader);
-    glUniform2i(textured_mapped_shader_resolution_uniform, target.width, target.height);
-    glUniform2f(textured_mapped_shader_pos_uniform, rect.x, rect.y);
-    glUniform2f(textured_mapped_shader_scale_uniform, rect.width, rect.height);
-    glUniform4f(textured_mapped_shader_uv_uniform, uv.x, uv.y, uv.width, uv.height);
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, tex.gl_reference);
-
-    glBindVertexArray(screen_quad_vao);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 static Texture to_texture(Bitmap bitmap, bool mipmaps)
@@ -471,6 +312,171 @@ VertexBuffer upload_vertex_buffer(Mesh mesh)
     }
 
     return ret;
+}
+
+static void draw_threed(RenderTarget target, Camera camera, glm::mat4 model, VertexBuffer buf, Material material, Texture cubemap, Texture env_map, Texture brdf_lut)
+{
+    glUseProgram(threed_shader);
+
+    unsigned int t_uniform = glGetUniformLocation(threed_shader, "t");
+    unsigned int model_uniform = glGetUniformLocation(threed_shader, "model");
+    unsigned int view_uniform = glGetUniformLocation(threed_shader, "view");
+    unsigned int projection_uniform = glGetUniformLocation(threed_shader, "projection");
+    unsigned int cam_pos_uniform = glGetUniformLocation(threed_shader, "cam_pos");
+    unsigned int albedo_tex_uniform = glGetUniformLocation(threed_shader, "albedo_tex");
+    unsigned int normal_tex_uniform = glGetUniformLocation(threed_shader, "normal_tex");
+    unsigned int metal_tex_uniform = glGetUniformLocation(threed_shader, "metal_tex");
+    unsigned int roughness_tex_uniform = glGetUniformLocation(threed_shader, "roughness_tex");
+    unsigned int irradiance_uniform = glGetUniformLocation(threed_shader, "irradiance");
+    unsigned int env_uniform = glGetUniformLocation(threed_shader, "env");
+    unsigned int brdf_uniform = glGetUniformLocation(threed_shader, "brdf");
+
+    static float t = 0.0f;
+    t += 0.01f;
+    glUniform1f(t_uniform, t);
+
+    glUniformMatrix4fv(model_uniform, 1, GL_FALSE, &model[0][0]);
+    glUniformMatrix4fv(view_uniform, 1, GL_FALSE, &camera.view[0][0]);
+    glUniformMatrix4fv(projection_uniform, 1, GL_FALSE, &camera.perspective[0][0]);
+    glUniform3f(cam_pos_uniform, camera.pos_x, camera.pos_y, camera.pos_z);
+
+    glUniform1i(albedo_tex_uniform, 0);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, material.albedo.gl_reference);
+    glUniform1i(normal_tex_uniform, 1);
+    glActiveTexture(GL_TEXTURE0 + 1);
+    glBindTexture(GL_TEXTURE_2D, material.normal.gl_reference);
+    glUniform1i(metal_tex_uniform, 2);
+    glActiveTexture(GL_TEXTURE0 + 2);
+    glBindTexture(GL_TEXTURE_2D, material.metal.gl_reference);
+    glUniform1i(roughness_tex_uniform, 3);
+    glActiveTexture(GL_TEXTURE0 + 3);
+    glBindTexture(GL_TEXTURE_2D, material.roughness.gl_reference);
+    glUniform1i(irradiance_uniform, 4);
+    glActiveTexture(GL_TEXTURE0 + 4);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap.gl_reference);
+    glUniform1i(env_uniform, 5);
+    glActiveTexture(GL_TEXTURE0 + 5);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, env_map.gl_reference);
+    glUniform1i(brdf_uniform, 6);
+    glActiveTexture(GL_TEXTURE0 + 6);
+    glBindTexture(GL_TEXTURE_2D, brdf_lut.gl_reference);
+
+    glBindVertexArray(buf.vao);
+    glDrawArrays(GL_TRIANGLES, 0, buf.vert_count);
+}
+
+static void draw_bar(RenderTarget target, Camera camera, glm::mat4 model, VertexBuffer buf, Material material, Texture cubemap, Texture env_map, Texture brdf_lut, Texture text)
+{
+    glUseProgram(bar_shader);
+
+    unsigned int t_uniform = glGetUniformLocation(bar_shader, "t");
+    unsigned int model_uniform = glGetUniformLocation(bar_shader, "model");
+    unsigned int view_uniform = glGetUniformLocation(bar_shader, "view");
+    unsigned int projection_uniform = glGetUniformLocation(bar_shader, "projection");
+    unsigned int cam_pos_uniform = glGetUniformLocation(bar_shader, "cam_pos");
+    unsigned int albedo_tex_uniform = glGetUniformLocation(bar_shader, "albedo_tex");
+    unsigned int normal_tex_uniform = glGetUniformLocation(bar_shader, "normal_tex");
+    unsigned int metal_tex_uniform = glGetUniformLocation(bar_shader, "metal_tex");
+    unsigned int roughness_tex_uniform = glGetUniformLocation(bar_shader, "roughness_tex");
+    unsigned int irradiance_uniform = glGetUniformLocation(bar_shader, "irradiance");
+    unsigned int env_uniform = glGetUniformLocation(bar_shader, "env");
+    unsigned int brdf_uniform = glGetUniformLocation(bar_shader, "brdf");
+    unsigned int text_tex_uniform = glGetUniformLocation(bar_shader, "text_tex");
+
+    static float t = 0.0f;
+    t += 0.01f;
+    glUniform1f(t_uniform, t);
+
+    glUniformMatrix4fv(model_uniform, 1, GL_FALSE, &model[0][0]);
+    glUniformMatrix4fv(view_uniform, 1, GL_FALSE, &camera.view[0][0]);
+    glUniformMatrix4fv(projection_uniform, 1, GL_FALSE, &camera.perspective[0][0]);
+    glUniform3f(cam_pos_uniform, camera.pos_x, camera.pos_y, camera.pos_z);
+
+    glUniform1i(albedo_tex_uniform, 0);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, material.albedo.gl_reference);
+    glUniform1i(normal_tex_uniform, 1);
+    glActiveTexture(GL_TEXTURE0 + 1);
+    glBindTexture(GL_TEXTURE_2D, material.normal.gl_reference);
+    glUniform1i(metal_tex_uniform, 2);
+    glActiveTexture(GL_TEXTURE0 + 2);
+    glBindTexture(GL_TEXTURE_2D, material.metal.gl_reference);
+    glUniform1i(roughness_tex_uniform, 3);
+    glActiveTexture(GL_TEXTURE0 + 3);
+    glBindTexture(GL_TEXTURE_2D, material.roughness.gl_reference);
+    glUniform1i(irradiance_uniform, 4);
+    glActiveTexture(GL_TEXTURE0 + 4);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap.gl_reference);
+    glUniform1i(env_uniform, 5);
+    glActiveTexture(GL_TEXTURE0 + 5);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, env_map.gl_reference);
+    glUniform1i(brdf_uniform, 6);
+    glActiveTexture(GL_TEXTURE0 + 6);
+    glBindTexture(GL_TEXTURE_2D, brdf_lut.gl_reference);
+    glUniform1i(text_tex_uniform, 7);
+    glActiveTexture(GL_TEXTURE0 + 7);
+    glBindTexture(GL_TEXTURE_2D, text.gl_reference);
+
+    glBindVertexArray(buf.vao);
+    glDrawArrays(GL_TRIANGLES, 0, buf.vert_count);
+}
+
+static void draw_rect(RenderTarget target, Rect rect, Color color)
+{
+    unsigned int resolution_uniform = glGetUniformLocation(basic_shader, "resolution");
+    unsigned int pos_uniform = glGetUniformLocation(basic_shader, "pos");
+    unsigned int scale_uniform = glGetUniformLocation(basic_shader, "scale");
+    unsigned int color_uniform = glGetUniformLocation(basic_shader, "color");
+
+    glUseProgram(basic_shader);
+    glUniform2i(resolution_uniform, target.width, target.height);
+    glUniform2f(pos_uniform, rect.x, rect.y);
+    glUniform2f(scale_uniform, rect.width, rect.height);
+    glUniform4f(color_uniform, color.r, color.g, color.b, color.a);
+
+    glBindVertexArray(screen_quad_vao);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+
+static void draw_textured_rect(RenderTarget target, Rect rect, Color color, Texture tex)
+{
+    unsigned int resolution_uniform = glGetUniformLocation(textured_shader, "resolution");
+    unsigned int pos_uniform = glGetUniformLocation(textured_shader, "pos");
+    unsigned int scale_uniform = glGetUniformLocation(textured_shader, "scale");
+    unsigned int color_uniform = glGetUniformLocation(textured_shader, "color");
+
+    glUseProgram(textured_shader);
+    glUniform2i(resolution_uniform, target.width, target.height);
+    glUniform2f(pos_uniform, rect.x, rect.y);
+    glUniform2f(scale_uniform, rect.width, rect.height);
+    glUniform4f(color_uniform, color.r, color.g, color.b, color.a);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex.gl_reference);
+
+    glBindVertexArray(screen_quad_vao);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+
+static void draw_textured_mapped_rect(RenderTarget target, Rect rect, Rect uv, Texture tex)
+{
+    int textured_mapped_shader_resolution_uniform = glGetUniformLocation(textured_mapped_shader, "resolution");
+    int textured_mapped_shader_pos_uniform = glGetUniformLocation(textured_mapped_shader, "pos");
+    int textured_mapped_shader_scale_uniform = glGetUniformLocation(textured_mapped_shader, "scale");
+    int textured_mapped_shader_uv_uniform = glGetUniformLocation(textured_mapped_shader, "uv");
+
+    glUseProgram(textured_mapped_shader);
+    glUniform2i(textured_mapped_shader_resolution_uniform, target.width, target.height);
+    glUniform2f(textured_mapped_shader_pos_uniform, rect.x, rect.y);
+    glUniform2f(textured_mapped_shader_scale_uniform, rect.width, rect.height);
+    glUniform4f(textured_mapped_shader_uv_uniform, uv.x, uv.y, uv.width, uv.height);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex.gl_reference);
+
+    glBindVertexArray(screen_quad_vao);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 Texture hdri_to_cubemap(Texture hdri, int size)
@@ -651,6 +657,6 @@ Texture generate_brdf_lut(int size)
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     glBindRenderbuffer(GL_RENDERBUFFER, 0);
     glViewport(0, 0, 1920, 1080);
-    
+
     return target;
 }
